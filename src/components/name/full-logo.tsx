12f366f6a5e8b8183674ead63bdedec90a9afb26
Zwane71 <makhohlisatemba@gmail.com>
2024-08-17 13:31:23 +0200

    adding background

---
+++ src/components/name/full-logo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+
+const Fulllogo = () => {
+	return (
+		<div>
+			<Image src='/full-logo.png' alt='Logo' width={1040} height={1040} />
+		</div>
+	);
+};
+
+export default Fulllogo;
---
+++ src/components/name/name.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Imagen from "../logo/name";
+
+const Name = () => {
+	return (
+		<div>
+			<Imagen />
+		</div>
+	);
+};
+
+export default Name;
